c8ddcd5ece5b565bb075d1b302704a556b890824
cynthia41 <cynthiaadhiambo41@gmail.com>
2023-03-25 15:53:51 +0300

    Completed mini project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target['new-task-description'].value)
+    
+  })
 });
+function buildToDo(task) {
+ let p = document.createElement('p')
+ let btn = document.createElement('button')
+ btn.addEventListener('click', handleDelete)
+ btn.textContent= 't'
+ p.textContent=task
+ p.appendChild(btn)
+ console.log(p)
+ document.querySelector('#tasks').appendChild(p)
+
+}
+function handleDelete (e){
+  e.target.parentNode.remove()
+}
